20b2bffb0ce24d75190a2ba8d1bc05452e2b931a
Chris Wade <swade781977@yahoo.com>
2020-02-24 10:46:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,9 @@ class Student
       WHERE grade != 12
     SQL
     
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def self.first_X_students_in_grade_10(num)
@@ -79,7 +81,9 @@ class Student
       LIMIT 1 
     SQL
     
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def self.all_students_in_grade_X(grade)

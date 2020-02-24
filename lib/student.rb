86b5d54f4fab2a2d8184e9d9c8aafd95a72e113c
Chris Wade <swade781977@yahoo.com>
2020-02-24 09:33:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
   end
 
   def self.all
-    sql =-SQL
+    sql = <<-SQL
       SELECT *
       FROM students 
     SQL
@@ -24,7 +24,7 @@ class Student
   end
 
   def self.find_by_name(name)
-    sql =-SQL
+    sql = <<-SQL
       SELECT *
       FROM students
       WHERE name = ?
